5ab27eaedb59c200a0d152be4b0325f9ba835b5b
Marius Begby <mariusbegby@gmail.com>
2023-03-13 18:45:28 +0100

    Setup react router with empty paths and components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import Search from './components/Search';
+import Results from './components/Results';
+import Movie from './components/Movie';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Routes>
+            <Route element={<Layout />}>
+                <Route index element={<Search />} />
+                <Route path='/results' element={<Results />} />
+                <Route path='/movie/:id' element={<Movie />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
